a37a369c370c1048d463b945397ae4cb7f1098a1
Kirill <shkolenko02-03-2004@mail.ru>
2023-01-29 13:06:49 +0300

    Improve Map

--- frontend/src/components/LeafletMap.vue
+++ frontend/src/components/LeafletMap.vue
@@ -5,33 +5,60 @@
 </template>
 
 <script>
+import login from "@/views/Login.vue";
+import logo from '../assets/logo.png'
 export default {
   name: "LeafletMap",
   props:{
-    zoom:'',
-    center:'',
-    markers:[],
+    zoom: {
+      type: Number,
+      default: 10,
+    },
+    center:{
+      type: Array,
+      default: [51.505, -0.09]
+    },
+    markers:{
+      type: Array,
+      default: [],
+    },
 
   },
   setup() {
     let map
-    let marker
-    let marker2
     return {
       map,
-      marker,
-      marker2,
       initMap() {
         map = L.map('map').setView([51.505, -0.09], 13)
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map)
-        marker = L.marker([51.5, -0.09])
-        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-        marker.addTo(map)
-        marker2 = L.marker([51.51, -0.09]).addTo(map)
 
-        marker2.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+        const icon = L.icon({
+          iconUrl: logo,
+          iconSize: [38, 95], // size of the icon
+          iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+          popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+        })
+
+        map.on('click', (e) => {
+          let marker = L.marker(e.latlng, {icon: icon}).addTo(map)
+        })
+
+        // marker1 = L.marker([51.5, -0.09])
+        // marker1.bindPopup("<b>Hello world!</b><br>I am a new popup.").openPopup();
+        // marker1.setIcon(icon)
+        //
+        // marker2 = L.marker([51.51, -0.09])
+        // marker2.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+        // this.markers.push(marker1)
+        // this.markers.push(marker2)
+
+
+        for (const index in this.markers){
+          this.markers[index].addTo(map)
+        }
+
       }
     }
   },
