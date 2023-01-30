b8a3de272494d8f28830b88c63d4a019311af002
Kirill <shkolenko02-03-2004@mail.ru>
2023-01-30 21:35:52 +0300

    The Captcha file

---
+++ frontend/src/views/Captcha.vue
@@ -0,0 +1,37 @@
+<template>
+  <VueRecaptcha
+    :sitekey="sitekey"
+    :load-recaptcha-script="true"
+    @verify="handleSuccess"
+    @error="handleError"
+  ></VueRecaptcha>
+</template>
+
+<script>
+import {VueRecaptcha} from "vue-recaptcha";
+
+export default {
+  name: "Captcha",
+  components: {
+    VueRecaptcha
+  },
+  data(){
+    return {
+      captcha: null,
+      sitekey: '6LfT_jskAAAAANglfKqAVEHFR9w5DGcu7qz3ZxXc'
+    }
+  },
+  methods:{
+    handleSuccess(response){
+      console.log(response)
+      this.captcha = true
+    },
+    handleError(){
+      console.log()
+    },
+  }
+}
+</script>
+
+<style scoped>
+</style>
